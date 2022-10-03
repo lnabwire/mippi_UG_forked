c51aed60bd586a095e4b1f15eec15f3dc22ecd59
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2022-10-03 18:46:06 +0200

    redid power calcs for factorial design

--- study_design/power_analysis.R
+++ study_design/power_analysis.R
@@ -13,12 +13,16 @@ library(lmtest)
 library(car)
 library(clubSandwich)
 set.seed(1492023) #set seed to date
-(setVar <- iccRE(ICC = 0.15, dist = "binary"))
+(setVar <- iccRE(ICC = 0.10, dist = "binary"))
 #run in /home/bjvca/data/projects/OneCG/MIPP/study_design/
-N_villages <- seq(from=10, to=80, by=5)     # The sample sizes we'll be considering
+N_villages <- seq(from=60, to=120, by=5)     # The sample sizes we'll be considering
 n_farmers_per_village <- seq(from=10, to=30, by=5)
 power <- matrix(NA, length(N_villages),length(n_farmers_per_village))           # Empty object to collect simulation estimates
-    
+power_H1 <- matrix(NA, length(N_villages),length(n_farmers_per_village))           # Empty object to collect simulation estimates
+power_H2 <- matrix(NA, length(N_villages),length(n_farmers_per_village))           # Empty object to collect simulation estimates
+power_H3 <- matrix(NA, length(N_villages),length(n_farmers_per_village))           # Empty object to collect simulation estimates
+power_one <- matrix(NA, length(N_villages),length(n_farmers_per_village))           # Empty object to collect simulation estimates
+
 alpha <- 0.05                                    # Standard significance level
 sims <- 1000                                    # Number of simulations to conduct for each N
 
@@ -29,7 +33,11 @@ for (j in 1:length(N_villages)){
     N1 <- n_farmers_per_village[k]
     
     significant.experiments <- rep(NA, sims)         # Empty object to count significant experiments
-
+    significant.experiments_H1 <- rep(NA, sims)         # Empty object to count significant experiments
+    significant.experiments_H2 <- rep(NA, sims)         # Empty object to count significant experiments
+    significant.experiments_H3 <- rep(NA, sims)         # Empty object to count significant experiments
+    significant.experiments_one <- rep(NA, sims)         # Empty object to count significant experiments
+    store_coefs <- matrix(NA,sims,4) 
     
     #### Inner loop to conduct experiments "sims" times over for each N ####
     for (i in 1:sims){
@@ -46,31 +54,51 @@ for (j in 1:length(N_villages)){
       dta <- genCluster(dtClust = dT, cLevelVar = "grp", 
                         numIndsVar = "size", level1ID = "id")
       dta <- addColumns(a, dta)
-    #  dta$y1 <- dta$y1+.75
+   dta$y1 <- dta$y1-1
       dta$y2 <- dta$y1+.75
+      dta$y3 <- dta$y1+.75
+      dta$y4 <- dta$y1+(3*1)
       
       dta$y1 <- dta$y1 > 0
       dta$y2 <- dta$y2 > 0
+      dta$y3 <- dta$y3 > 0
+      dta$y4 <- dta$y4 > 0
       
       #put half of the groups in a treatment group
-      dta$treat <- 0
-      dta$treat[dta$grp %in% sample(names(table(dta$grp)), size = N/2)] <- 1
+      dta$treat_seed <- 0
+      dta$treat_cons <- 0
+      all <- names(table(dta$grp))
+      sampl_1 <- sample(names(table(dta$grp)), size = N/4)
+      sampl_2 <-  sample(setdiff(all, sampl_1), size = N/4)
+      sampl_3 <-  sample(setdiff(setdiff(all, sampl_1),sampl_2), size = N/4)
+      dta$treat_seed[dta$grp %in% c(sampl_1,sampl_3)] <- 1
+      dta$treat_cons[dta$grp %in% c(sampl_2,sampl_3)] <- 1
       
-      dta$y <- dta$y2*dta$treat + dta$y1*(1-dta$treat)
+    
+      dta$y <- dta$y2*(dta$treat_seed==1 & dta$treat_cons==0)+ dta$y3*(dta$treat_cons==1 & dta$treat_seed==0)+ (dta$y4)*(dta$treat_seed==1 & dta$treat_cons==1) + dta$y1*(dta$treat_seed==0 & dta$treat_cons==0)
       
-      fit.sim <- lm(y~treat,data=dta)
+      fit.sim <- lm(y~treat_seed*treat_cons,data=dta)
       summary(fit.sim)
       vcov_cluster <- vcovCR(fit.sim,cluster=dta$grp,type="CR3")
       coef_test( fit.sim , vcov_cluster)$p_Satt
-      
+      store_coefs[i,] <-  coef(fit.sim)
       p.value <- summary(fit.sim)$coefficients[2,4]  # Extract p-values
-      p.value<-  coef_test( fit.sim , vcov_cluster)$p_Satt[2]  # Extract p-values
-     
-      significant.experiments[i] <- p.value < alpha 
-      # print(c(p.value, p.value_i, p.value_i2))
+      p.value_1<-  coef_test( fit.sim , vcov_cluster)$p_Satt[2]  # Extract p-values
+      p.value_2<-  coef_test( fit.sim , vcov_cluster)$p_Satt[3]  # Extract p-values
+      p.value_3<-  coef_test( fit.sim , vcov_cluster)$p_Satt[4]  # Extract p-values
+      significant.experiments[i] <- (p.value_1 < alpha) & (p.value_2 < alpha) & (p.value_3 < alpha)
+      significant.experiments_H1[i] <- (p.value_1 < alpha)
+      significant.experiments_H2[i] <- (p.value_2 < alpha)
+      significant.experiments_H3[i] <- (p.value_3 < alpha)
+      significant.experiments_one[i] <- (p.value_1 < alpha) | (p.value_2 < alpha) | (p.value_3 < alpha)
+  
     }
     
     power[j,k] <- mean(significant.experiments)
+    power_H1[j,k] <- mean(significant.experiments_H1)    
+    power_H2[j,k] <- mean(significant.experiments_H2)    
+    power_H3[j,k] <- mean(significant.experiments_H3)   
+    power_one[j,k] <- mean(significant.experiments_one)  
     
   }
   # store average success rate (power) for each N
